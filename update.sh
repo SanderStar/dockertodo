875fac5627c9989884de025f3a5578b7216cbca6
Sander Star <sanderstar1@gmail.com>
2017-10-17 11:06:59 +0200

    Test shell script

--- update.sh
+++ update.sh
@@ -8,7 +8,10 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
+   $(lsb_release -cs) \sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
    stable"
 sudo apt-get update -y
 sudo apt-get install -y docker-ce
+
+# nodig?
+sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
