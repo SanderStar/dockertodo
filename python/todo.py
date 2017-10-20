24f26eac82019395e431be60cc19722fa3bf1a4e
Sander Star <sanderstar1@gmail.com>
2017-10-20 19:26:47 +0200

    template with variables

--- python/todo.py
+++ python/todo.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+app.mongo = MongoClient("mongodb://mongo:27017", connect=False).tododb
 
 @app.route("/todo.py")
 def todo():
-    return  render_template("todo.html")
+    items = app.mongo.tododb.find()
+    return  render_template("todo.html", **locals())
+
+@app.route("/todo.py/new")
+def new():
+ return render_template("todo.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=False)
