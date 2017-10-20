8d16a968a4445d52f4c4cbf18622a8d5405ad3f0
Sander Star <sanderstar1@gmail.com>
2017-10-20 08:24:38 +0200

    test

--- update.sh
+++ update.sh
@@ -15,3 +15,7 @@ sudo apt-get install -y docker-ce
 
 # nodig?
 sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# voortaan vermijden sudo
+sudo usermod -aG docker ubuntu
