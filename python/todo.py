c39d3aada644b40201f1516950071729568bef53
Sander Star <sanderstar1@gmail.com>
2017-10-20 12:56:38 +0200

    adding html templating for python

--- python/todo.py
+++ python/todo.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,7 +7,7 @@ def hello():
 
 @app.route("/todo.py")
 def todo():
-    return "<h1>TODO</h1>"
+    return  render_template("todo.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=False)
