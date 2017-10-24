3a2c99c3a963249e134729777c44d8278b93c20c
Sander Star <sanderstar1@gmail.com>
2017-10-24 16:26:38 +0200

    show current items

--- python/todo.py
+++ python/todo.py
@@ -3,15 +3,15 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-app.mongo = MongoClient("mongodb://mongo:27017", connect=False).tododb
+app.mongo = MongoClient("mongodb://mongodb:27017", connect=False).tododb
 
 @app.route("/todo.py")
 def todo():
     items = app.mongo.tododb.find()
-    return  render_template("todo.html", **locals())
+    return  render_template("todo.html", items=items)
 
 @app.route("/todo.py/new")
-def new():
+def newitem():
  return render_template("todo.html")
 
 if __name__ == "__main__":
