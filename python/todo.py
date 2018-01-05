bda4553e380130e74dfac5dfec244a274c0481c9
Sander Star <sanderstar@creetion.com>
2018-01-05 08:06:24 +0100

    Toevoegen todo's mbt python

--- python/todo.py
+++ python/todo.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -8,11 +8,16 @@ app.mongo = MongoClient("mongodb://mongodb:27017", connect=False).tododb
 @app.route("/todo.py")
 def todo():
     items = app.mongo.tododb.find()
-    return  render_template("todo.html", items=items)
+    return render_template("todo.html", items=items)
 
-@app.route("/todo.py/new")
+@app.route("/todo.py/new", methods=['POST'])
 def newitem():
- return render_template("todo.html")
+    name = request.form["name"]
+    description = request.form["description"]
+    # Toevoegen in database
+    data = [{ "name": name, "description": description}]
+    app.mongo.tododb.insert(data)
+    return redirect("/todo.py")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=False)
